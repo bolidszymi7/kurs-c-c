9e425f2421c4c11993f10767879db5929d1b8238
bolidszymi7 <zielinskib200@gmail.com>
2025-10-18 12:20:52 +0200

    Create 1.cpp
    t

---
+++ 1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+
+	double a, b, c, srednia;
+		cout << "podaj a ";
+		cin >> a; 
+		cout << "podaj b ";
+		cin >> b;
+		cout << "podaj c ";
+		cin >> c;
+		
+		srednia = (a + b + c);
+		
+		cout << "srednia; " << srednia / 3;
+
+		return 0;
+
+
+}
