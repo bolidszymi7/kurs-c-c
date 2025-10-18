b31a37e26270219fa963715d4b18bed4c5ea61af
bolidszymi7 <zielinskib200@gmail.com>
2025-10-18 14:58:10 +0200

    Create godzinki.cpp

---
+++ godzinki.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+
+int main()
+{
+	int sec, min, godz,reszta;
+	cout << "podaj sekundy:";
+	cin >> sec;
+	godz = sec / 3600;
+	min = sec/60-godz*60;
+	reszta = sec-min*60-godz*3600;
+			
+	cout << "czas wynosi:"<<godz<<"h" << min <<"min" << reszta <<"sec" << endl;
+	
+		
+
+	return 0;
