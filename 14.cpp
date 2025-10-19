3f74cbfe0193d345c3a6c31c7922247ad7fb5753
bolidszymi7 <zielinskib200@gmail.com>
2025-10-19 11:55:26 +0200

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int licz;
+	licz = 2;
+
+	while (licz <= 20) {
+		if(licz%2==0)
+		cout << licz<<"" << endl;
+		licz++;
+	}
+
+	return 0;
+}
