ac2f8208857a3a7ec87df7f356d670940f0bb1af
bolidszymi7 <zielinskib200@gmail.com>
2025-10-19 12:28:01 +0200

    Create 18.cpp

---
+++ 18.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int licz ;
+	
+	do {
+		cout << "podaj liczbe" << endl;
+		cin >> licz;
+	} while (licz != 0); {
+
+		}
+	
+
+	
+
+	return 0;
+}
