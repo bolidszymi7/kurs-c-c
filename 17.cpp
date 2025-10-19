51cc071598c86208b2dc89ac202f47ee0591b1cb
bolidszymi7 <zielinskib200@gmail.com>
2025-10-19 12:23:16 +0200

    Create 17.cpp

---
+++ 17.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int licz,li ;
+	
+	
+	for (int licz =1 ;licz <=10 ; licz++) {
+		
+		
+		li = licz * licz;
+		cout << li << endl;
+		
+	}
+
+	
+
+	return 0;
+}
