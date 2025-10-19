62a6deb49cea41e3e09b43a5fe9dbf96e8529484
bolidszymi7 <zielinskib200@gmail.com>
2025-10-19 12:03:01 +0200

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+	int licz;
+	
+	for (int licz = 100;licz >= 1; licz--) {
+		cout << licz << endl;
+		}
+
+	return 0;
+}
