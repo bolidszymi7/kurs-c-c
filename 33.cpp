610cab8eaa360c2c932c90e6418991ea7fb30fd8
bolidszymi7 <zielinskib200@gmail.com>
2025-10-25 15:54:39 +0200

    Create 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+
+double obw_kwa(float a){
+    return a*4;
+}
+
+
+
+int main() {
+double a;
+
+
+ cout<<"podaj liczbe:";
+ cin>> a;
+ 
+ cout<<"obwod kwadratu:"<<obw_kwa(a);
+     return 0;
+}
