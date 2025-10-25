d9bc23bafe84284cf92748c2ee0eef4760a15f24
bolidszymi7 <zielinskib200@gmail.com>
2025-10-25 14:25:29 +0200

    Create 25.cpp

---
+++ 25.cpp
@@ -0,0 +1,26 @@
+#include<iostream>      
+
+
+using namespace std;
+
+
+
+void kwadrat(int a) {
+    cout <<a*a <<endl;
+
+}
+
+
+
+
+int main() {
+    int a;
+    
+    cout << "podaj a:";
+    cin >> a;
+    
+    
+    kwadrat(a);
+
+    return 0;
+}
