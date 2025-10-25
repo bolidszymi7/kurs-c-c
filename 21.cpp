49655705b095d16e60bb74174591f25d5480e380
bolidszymi7 <zielinskib200@gmail.com>
2025-10-25 09:59:30 +0200

    Create 21.cpp

---
+++ 21.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main() {
+	int n;
+	cout << "podaj liczbe:";
+	cin >> n;
+
+	for (int i = 1; i <= 10; i++) {
+		
+		cout << i << "*" << n << "=" << i * n << endl;
+
+	}
+
+
+
+
+
+
+
+	return 0;
+}
